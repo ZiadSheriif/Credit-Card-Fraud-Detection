6721f8984a92051a6f6834322f1480d7710e13eb
ZiadSheriif <zsherif308@gmail.com>
2024-05-02 05:17:15 +0300

    Complete pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,8 +1,9 @@
 from preprocessing import *
 from data_splitter import *
-from train import *
+from model import *
 from visualization import *
 from feature_extraction import *
+from sklearn.model_selection import ShuffleSplit
 
 
 def run():
@@ -12,8 +13,8 @@ def run():
     
     # data distribution
     vis=DataDistribution(df.df)
-    vis.plot_class_distribution()
-    vis.plot_distributions()
+    # vis.plot_class_distribution()
+    # vis.plot_distributions()
     
     
     # Data scaling
@@ -27,22 +28,48 @@ def run():
     fe=FeatureExtraction(scaled_df)
     under_sampled_df= fe.undersample()
     
-    vis.plot_subsample(under_sampled_df)
-    vis.plot_corr(under_sampled_df)
-    vis.plot_neg_corr(under_sampled_df, ['V10', 'V12', 'V14', 'V17'])
-    vis.plot_pos_corr(under_sampled_df, ['V4', 'V2', 'V11', 'V19'])
-    vis.plot_fraud_dist(under_sampled_df, ['V10', 'V12', 'V14'])
+    # vis.plot_subsample(under_sampled_df)
+    # vis.plot_corr(under_sampled_df)
+    # vis.plot_neg_corr(under_sampled_df, ['V10', 'V12', 'V14', 'V17'])
+    # vis.plot_pos_corr(under_sampled_df, ['V4', 'V2', 'V11', 'V19'])
+    # vis.plot_fraud_dist(under_sampled_df, ['V10', 'V12', 'V14'])
     
     
     # remove outliers
-    features=['V14', 'V12', 'V10', ]
+    features=['V14', 'V12', 'V10']
     df.remove_outliers(under_sampled_df,features)
     
-    vis.plot_outlier_reduction(under_sampled_df, features)
+    # vis.plot_outlier_reduction(under_sampled_df, features)
     
     # TODOS: Complete the pipeline 
-    # x_red_tsne, x_red_pca, x_red_svd=fe.reduce_dimensions()
+    x_red_tsne, x_red_pca, x_red_svd=fe.reduce_dimensions()
     # vis.plot_clusters(x_red_tsne, x_red_pca, x_red_svd, under_sampled_df['Class'])
+    
+    # Train the model
+    train=ModelTrainer()
+    X = under_sampled_df.drop('Class', axis=1)
+    y = under_sampled_df['Class']
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+   # Turn the values into an array for feeding the classification algorithms.
+    X_train = X_train.values
+    X_test = X_test.values
+    y_train = y_train.values
+    y_test = y_test.values
+    
+    train.train_models(X_train, y_train)
+    train.undersample_and_evaluate(under_sampled_df)
+    
+    
+    # Evaluate the model
+    cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=42)
+    train.plot_learning_curve(X_train, y_train, cv=cv, n_jobs=4)
+    train.calculate_roc_auc_scores(X_train, y_train)
+    train.graph_roc_curve_multiple(X_train, y_train)
+    
+
+    
     
     
     
