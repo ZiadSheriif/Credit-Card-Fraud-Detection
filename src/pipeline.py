97a60bb85c2126b4e3d16c35b2cd939bc291e576
ZiadSheriif <zsherif308@gmail.com>
2024-05-02 05:19:47 +0300

    Add rest of data visualization

--- src/pipeline.py
+++ src/pipeline.py
@@ -13,8 +13,8 @@ def run():
     
     # data distribution
     vis=DataDistribution(df.df)
-    # vis.plot_class_distribution()
-    # vis.plot_distributions()
+    vis.plot_class_distribution()
+    vis.plot_distributions()
     
     
     # Data scaling
@@ -28,22 +28,22 @@ def run():
     fe=FeatureExtraction(scaled_df)
     under_sampled_df= fe.undersample()
     
-    # vis.plot_subsample(under_sampled_df)
-    # vis.plot_corr(under_sampled_df)
-    # vis.plot_neg_corr(under_sampled_df, ['V10', 'V12', 'V14', 'V17'])
-    # vis.plot_pos_corr(under_sampled_df, ['V4', 'V2', 'V11', 'V19'])
-    # vis.plot_fraud_dist(under_sampled_df, ['V10', 'V12', 'V14'])
+    vis.plot_subsample(under_sampled_df)
+    vis.plot_corr(under_sampled_df)
+    vis.plot_neg_corr(under_sampled_df, ['V10', 'V12', 'V14', 'V17'])
+    vis.plot_pos_corr(under_sampled_df, ['V4', 'V2', 'V11', 'V19'])
+    vis.plot_fraud_dist(under_sampled_df, ['V10', 'V12', 'V14'])
     
     
     # remove outliers
     features=['V14', 'V12', 'V10']
     df.remove_outliers(under_sampled_df,features)
     
-    # vis.plot_outlier_reduction(under_sampled_df, features)
+    vis.plot_outlier_reduction(under_sampled_df, features)
     
     # TODOS: Complete the pipeline 
     x_red_tsne, x_red_pca, x_red_svd=fe.reduce_dimensions()
-    # vis.plot_clusters(x_red_tsne, x_red_pca, x_red_svd, under_sampled_df['Class'])
+    vis.plot_clusters(x_red_tsne, x_red_pca, x_red_svd, under_sampled_df['Class'])
     
     # Train the model
     train=ModelTrainer()
